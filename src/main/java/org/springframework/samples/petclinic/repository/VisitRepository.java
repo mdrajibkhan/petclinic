19b5cd09cdf3bb95b8660eda1668fa5dcffda98c
mdrajibkhan <68429876+mdrajibkhan@users.noreply.github.com>
2021-10-03 15:37:22 +0600

    Update VisitRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/VisitRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/VisitRepository.java
@@ -16,7 +16,6 @@
 package org.springframework.samples.petclinic.repository;
 
 import java.util.List;
-import java.client;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.BaseEntity;
